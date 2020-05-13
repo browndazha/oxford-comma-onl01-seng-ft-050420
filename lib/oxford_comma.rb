d8a0ab360e1f2ae8b84f83944450bb666a2507d2
Dazha Brown <dajahbrown@hotmail.com>
2020-05-13 22:41:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,2 @@
 def oxford_comma(array)
-    if array.length==1
-        puts array[0]
-    else
-        last=array[array.length-1]
-        array.pop
-        string=array.join(',')
-        if array.length==1
-            string.concat(" and ")
-        else
-            string.concat(" and ")
-        end
-        string.concat(last)
-        puts string
-    end
-end
-
-items = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
-oxford_comma(items)
+end 
