c888d4f44141e36270bad168a68f89686a9255b4
Dazha Brown <dajahbrown@hotmail.com>
2020-05-14 01:20:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,19 @@
-def oxford_comma(array)
-  return "kiwi"
+require "pry"
+
+def oxford_comma(list_of_fruits)
+  binding.pry
+  # if array.size == 2
+  #   two = array.join(" and ")
+  #   two
 end
 
-def oxford_comma(array)
-  if array.size == 2
-    two = array.join(" and ")
-    two
-end
+# kiwi
+#
+# kiwi strawberry
+# kiwi and strawberry
+#
+# 
+# kiwi strawberry banana
+#
+# kiwi, peach, orange, strawberry, and banana
+# ["kiwi", "peach", "orange", "strawberry", "banana"]
