0810296415498c1d1fd7af1f1c9d44123c566577
Dazha Brown <dajahbrown@hotmail.com>
2020-05-14 01:51:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ require "pry"
 def oxford_comma(list_of_fruits)
   binding.pry
   if list_of_fruits.length == 1 # check to see if the length of the array is 1
+    list_of_fruits.join(', ')
     # if it is, convert the array to string and return string
 
   #elsif # check to see if the length of the array is 2
@@ -11,7 +12,7 @@ def oxford_comma(list_of_fruits)
 
   #elsif # What do we do if the array is > 2?
 
-  # end
+  end
 end
 
 # kiwi
