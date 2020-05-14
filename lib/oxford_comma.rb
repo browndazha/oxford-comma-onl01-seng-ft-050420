8c2fb678c02c04c45c371dcc602c520d7ac13fc5
Dazha Brown <dajahbrown@hotmail.com>
2020-05-14 01:38:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 require "pry"
 
 def oxford_comma(list_of_fruits)
+  binding.pry
   if # check to see if the length of the array is 1
     # if it is, convert the array to string and return string
 
